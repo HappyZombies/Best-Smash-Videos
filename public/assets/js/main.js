0943792f316c709f729285f2215550085dac7c43
Daniel <daniel.reguero@hotmail.com>
2016-08-03 17:49:34 -0400

    Gave up on custom upvote, used the upvote plugin instead.
    I tried making it good but I gave up, so I just downloaded a plugin
    instead. Now to work around this and make it work for me.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,86 +1,19 @@
 $(document).ready(function(){
 	console.log("I am from JQuery!");
-	// $('form').on('submit', function(){
-	// 	var item = $('form input');
-	// 	var todo = {item: item.val()};
 
-	// 	$.ajax({
-	// 		type: 'POST',
-	// 		url: '/todo',
-	// 		data: todo,
-	// 		success: function(data){
-	// 			location.reload();
-	// 		}
-	// 	});
-	// 	return false;
-	// });
-	// $('#upvote span').on('click', function(){
-	// 	console.log("I was clicked");
-	// 	//why is this being dumb ? 
-	// 	if($(this).context.nextElementSibling == null){
-	// 		console.log("Bye");
-	// 		return;
-	// 	}
-	// 	$(this).context.nextElementSibling.innerText = parseInt($(this).context.nextElementSibling.innerText) + 1; 
-	// 	var add = {votes: 1};
-	// 	$.ajax({
-	// 		type: 'POST',
-	// 		url: '/',
-	// 		data: add,
-	// 		success: function(data){
-	// 			console.log("Success...now wat...."+data);
-	// 		}
-	// 	});
-	// });
-
-	var upvoted = false;
-	var downvoted = false;
-	$(document).on("click", function(e){
-
-		var thisId = "#"+e.target.id;
-		var add = 1;
-
-		if(thisId.indexOf("#upvote-") >= 0){
-			if(!upvoted){
-				if(downvoted){
-					console.log("This is already downvoted, better change that");
-					var downvoteID = $(thisId)[0].parentNode.nextElementSibling.nextElementSibling.lastChild.id;
-					console.log(downvoteID);
-					$("#"+downvoteID).css('color', '#6583A6');
-					downvoted = false;
-					add = 2;
-				}
-				upvoted = true;
-				$(thisId).css('color', '#053dc7');
-				$(thisId)[0].parentNode.nextElementSibling.innerHTML = parseInt($(thisId)[0].parentNode.nextElementSibling.innerHTML) + add;
-			}else{
-				upvoted = false;
-				$(thisId).css('color', '#6583A6');
-				$(thisId)[0].parentNode.nextElementSibling.innerHTML = parseInt($(thisId)[0].parentNode.nextElementSibling.innerHTML) - add;
-			}
-		}
-
-		else if(thisId.indexOf("#downvote-") >= 0){
-			if(!downvoted){
-				if(upvoted){
-					console.log("This is already upvoted, better update that");
-					var upvoteID = "#"+$(thisId)[0].parentNode.previousElementSibling.previousElementSibling.lastChild.id;
-					$(upvoteID).css('color','#6583A6');
-					upvoted = false;
-					add = 2;
-				}
-				downvoted = true;
-				$(thisId).css('color', '#B61420');
-				$(thisId)[0].parentNode.previousElementSibling.innerHTML = parseInt($(thisId)[0].parentNode.previousElementSibling.innerHTML) - add;
-			}else{
-				downvoted = false;
-				$(thisId).css('color', '#6583A6');
-				$(thisId)[0].parentNode.previousElementSibling.innerHTML = parseInt($(thisId)[0].parentNode.previousElementSibling.innerHTML) + add;
-			}
-		}
-
+	var callback = function(data) {
+		$.ajax({
+			url: window.location.href,
+			type: 'post',
+			data: { id: data.id, up: data.upvoted, down: data.downvoted, star: data.starred } //Add Correct Data.
+		});
+	};
+	
+	
+	$('.video').each(function(){
+		var numberID = this.id.replace( /^\D+/g, '');
+		$(this.firstElementChild).upvote();
 	});
-
 
 	//Interesting function https://www.sitepoint.com/display-youtube-thumbnails-easy-jquery/
 	$.extend({
